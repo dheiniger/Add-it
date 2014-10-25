6fb7fa20cd633bdc3f2c7ff4a72b2bf5ea8c6592
Daniel <runaway@Daniel.(none)>
2014-10-25 16:27:49 -0500

    Create GUI prototype

--- src/com/runaway/view/Gui.java
+++ src/com/runaway/view/Gui.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 import com.runaway.model.Parser;
+import java.awt.TextArea;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 
 public class Gui {
 	public Gui(Parser p){
@@ -17,14 +20,27 @@ public class Gui {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		addPanes(f);
+		f.getContentPane().setLayout(null);
+		
+		TextArea textArea = new TextArea();
+		textArea.setBounds(10, 10, 317, 1001);
+		f.getContentPane().add(textArea);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(333, 615, 117, 25);
+		f.getContentPane().add(btnNewButton);
+		
+		TextArea textArea_1 = new TextArea();
+		textArea_1.setBounds(444, 10, 394, 1030);
+		f.getContentPane().add(textArea_1);
 //		addButton();
 	}
 	
 	private void addPanes(JFrame frame){
-		JTextArea leftBox = new JTextArea(60,  50);
-		JTextArea rightBox = new JTextArea(60,  50);
-		
-		frame.add(leftBox);
-		frame.add(rightBox);
+//		JTextArea leftBox = new JTextArea(60,  50);
+//		JTextArea rightBox = new JTextArea(60,  50);
+//		
+//		frame.add(leftBox);
+//		frame.add(rightBox);
 	}
 }
